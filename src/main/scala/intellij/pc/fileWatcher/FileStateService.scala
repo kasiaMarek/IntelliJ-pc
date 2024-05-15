17131fdf1ce7a7eeced9d2e785eab083c4a3c5cb
Katarzyna Marek <kasia@marek.net>
2024-05-15 15:48:16 +0200

    restart pc when needed

---
+++ src/main/scala/intellij/pc/fileWatcher/FileStateService.scala
@@ -0,0 +1,26 @@
+package intellij.pc.fileWatcher
+
+import java.util.concurrent.atomic.AtomicReference
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.vfs.VirtualFile
+import intellij.pc.PresentationCompilerPluginService
+
+class FileStateService(project: Project) {
+  private val modifiedFiles = new AtomicReference[Set[VirtualFile]](Set.empty)
+
+  def inFocusChanged(): Unit = {
+    val modules = modifiedFiles.getAndSet(Set.empty).flatMap { file =>
+      Option(ProjectRootManager.getInstance(project).getFileIndex.getModuleForFile(file))
+    }
+    if(modules.nonEmpty) {
+      project.getService(classOf[PresentationCompilerPluginService]).restarPc(modules)
+    }
+  }
+
+  def modified(vFile: VirtualFile): Unit = {
+    modifiedFiles.updateAndGet(_ + vFile)
+  }
+
+}
